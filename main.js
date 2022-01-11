0055ccdf513e493a53850dc359843c33e0cf7b75
AFCMS <afcm.contact@gmail.com>
2022-01-11 21:26:22 +0100

    fixes

--- main.js
+++ main.js
@@ -2,24 +2,29 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
 function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      //preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			//preload: path.join(__dirname, 'preload.js')
+		},
+	})
 
-  mainWindow.loadFile('index.html')
+	mainWindow.setMenu(null)
+
+	mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+	createWindow()
+	app.on('activate', function () {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
+}).then(() => {
+	//new Notification({ title: "Help", body: "chronotimer" }).onclick(() => console.log("msg")).show()
+	new Notification({ title: "Help", body: "chronotimer" }).show()
 })
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
